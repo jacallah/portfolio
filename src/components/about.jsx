77cc37daa384ed06244e179a3799bd134f6ac3a4
Jack Callahan <jacallah@mtu.edu>
2022-05-20 13:54:29 -0400

    initial commit

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,10 +11,10 @@ export default class About extends Component {
                 <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
                 <div className="col-md-12">
                     <div className="about-desc">
-                    <span className="heading-meta">About Us</span>
+                    <span className="heading-meta">About Me</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                    <p>I studied computer science at Michgan Technological University and a minored in cyber security. I have a passion for programming new projects and developing strong security practices.</p>
+                    <p>In my free time, I play sports such as soccer and golf along with playing jazz saxophone.</p>
                     </div>
                 </div>
                 </div>
@@ -26,8 +26,8 @@ export default class About extends Component {
         <div className="colorlib-narrow-content">
             <div className="row">
             <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-                <span className="heading-meta">What I do?</span>
-                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+                <span className="heading-meta">What have I accomplished?</span>
+                <h2 className="colorlib-heading">Here are some of my experiences</h2>
             </div>
             </div>
             <div className="row row-pt-md">
@@ -38,7 +38,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Web Development </h3>
-                    <p>I have experience building websites and chrome extentions using JavaScript,React,HTML,CSS</p>
+                    <p>I have experience building websites and using JavaScript, React, NodeJS, HTML, CSS, PHP</p>
                 </div>
                 </div>
             </div>
@@ -48,8 +48,8 @@ export default class About extends Component {
                     <i className="icon-phone3" />
                 </span>
                 <div className="desc">
-                    <h3>Data Structures & Algorithms</h3>
-                    <p>As coming from the CS background, I have good grasp over fundamental concepts of DSA</p>
+                    <h3>Data Structures, Algorithms, & Programming Languages</h3>
+                    <p>As a CS student, I have a background in common data structure practices, algorithm efficiency, and a deep understaing of programming languages</p>
                 </div>
                 </div>
             </div>
@@ -59,8 +59,8 @@ export default class About extends Component {
                     <i className="icon-data" />
                 </span>
                 <div className="desc">
-                    <h3>Dev Ops</h3>
-                    <p>I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
+                    <h3>Cyber Security</h3>
+                    <p>I had a role as a Security Operations Intern at a fortune 400 company and course work in cryptography and standard security practices</p>
                 </div>
                 </div>
             </div>
